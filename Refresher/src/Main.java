4dbc042e862af48d85e2b0ec476b80322d29abbd
Frank Daniel <mbuthiawanganju@gmail.com>
2023-12-27 11:27:51 -1200

    I'm trying to understand how to use github with eclipse. This is the initial commit

---
+++ Refresher/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {	
+	public static void main(String[] args) {
+		
+		Scanner read=new Scanner(System.in);
+		int num=100;
+		for(int i=100; i>=0;i--) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
